d01551dbad9d727e90fe65d8e534f965c3a9af38
kuznetsov_rs <kuznetsov_rs@ooo.technology>
2024-02-14 16:16:41 +0500

    ОбновлениеЭ

---
+++ app/yay/page.js
@@ -0,0 +1,10 @@
+export default function page() {
+
+  return (
+    <div className='box'>
+      <img alt='bugcat yayyy' className='image' src='/bugcat-yay.gif' />
+      Я тебя люблю 🥰
+    </div>
+  )
+
+}
